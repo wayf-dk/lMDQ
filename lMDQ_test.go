3f2064a8b642dad9601f93b4e6b66b0a6e762374
Mehran Khan <mekhan@test-wayf-03.test.lan>
2018-05-24 12:23:43 +0000

    New tests Added.

--- lMDQ_test.go
+++ lMDQ_test.go
@@ -1,49 +1,76 @@
 package lMDQ
 
 import (
-	"log"
+	"crypto/sha1"
+	"encoding/base64"
+	"fmt"
+	"github.com/wayf-dk/gosaml"
+	"github.com/wayf-dk/goxml"
 	"os"
 	"testing"
 )
 
-var (
-	hashes                                                                  = []string{}
-	test_hub, test_hub_ops, test_edugain, prod_hub, prod_hub_ops, prod_birk *MDQ
+type (
+	MdSets struct {
+		Hub, Internal, ExternalIdP, ExternalSP gosaml.Md
+	}
 )
 
-func xTestMain(m *testing.M) {
+var (
+	Md MdSets
+)
 
+func printHashedDom(xp *goxml.Xp) {
+	hash := sha1.Sum([]byte(xp.C14n(nil, "")))
+	fmt.Println(base64.StdEncoding.EncodeToString(append(hash[:])))
+}
+
+// Need to change Path. So it should work for everyone.
+func TestMain(m *testing.M) {
+	Md.Hub = &MDQ{Path: "file:/home/mekhan/wayfhybrid/hybrid-metadata-test.mddb?mode=ro", Table: "HYBRID_HUB"}
+	Md.Internal = &MDQ{Path: "file:/home/mekhan/wayfhybrid/hybrid-metadata.mddb?mode=ro", Table: "HYBRID_INTERNAL"}
+	Md.ExternalIdP = &MDQ{Path: "file:/home/mekhan/wayfhybrid/hybrid-metadata-test.mddb?mode=ro", Table: "HYBRID_EXTERNAL_IDP"}
+	Md.ExternalSP = &MDQ{Path: "file:/home/mekhan/wayfhybrid/hybrid-metadata.mddb?mode=ro", Table: "HYBRID_EXTERNAL_SP"}
+
+	for _, md := range []gosaml.Md{Md.Hub, Md.Internal, Md.ExternalIdP, Md.ExternalSP} {
+		err := md.(*MDQ).Open()
+		if err != nil {
+			panic(err)
+		}
+	}
 	os.Exit(m.Run())
 }
 
-func xgetBenchmarkHashes() {
-	hash := ""
-	rows, err := prod_hub_ops.db.Query("select l.hash from entity e, lookup l where e.id = l.entity_id_fk order by l.hash")
-	if err != nil {
-		log.Panicln(err)
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		rows.Scan(&hash)
-		hashes = append(hashes, hash)
-	}
+func ExampleMDQ() {
+	extMetadata, _ := Md.ExternalIdP.MDQ("https://aai-logon.switch.ch/idp/shibboleth")
+	printHashedDom(extMetadata)
+	intMetadata, _ := Md.Internal.MDQ("https://wayfsp.wayf.dk")
+	printHashedDom(intMetadata)
+	hubMetadata, _ := Md.Hub.MDQ("https://wayf.wayf.dk")
+	printHashedDom(hubMetadata)
+	// Output:
+	// HOuViozZYV3I9xTnCTMlpRfrAiU=
+	// g2R+ZRYDc+mrY6mvgMKaFyn4M/0=
+	// bHo3f2bGLC3YWq16SgancRECE04=
 }
 
-func xTestAll(t *testing.T) {
-	xp, err := test_hub_ops.MDQ("")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	log.Println(xp.Doc.Dump(true))
+func ExampleNoMetadata() {
+	_, err := Md.ExternalIdP.MDQ("https://attribute-viewer.aai.switch.ch/interfederation-test/shibboleth")
+	fmt.Println(err)
+	// Output:
+	// ["cause:Metadata not found","err:Metadata not found","key:https://attribute-viewer.aai.switch.ch/interfederation-test/shibboleth","table:HYBRID_EXTERNAL_IDP"]
 }
 
-func xBenchmarkMDQ(b *testing.B) {
-	xgetBenchmarkHashes()
-	max := len(hashes)
+func ExampleDbget() {
+	extMetadata, _ := Md.ExternalIdP.(*MDQ).dbget("https://aai-logon.switch.ch/idp/shibboleth", true)
+	printHashedDom(extMetadata)
+	// Output:
+	// HOuViozZYV3I9xTnCTMlpRfrAiU=
+}
 
-	for i := 0; i < b.N; i++ {
-		hash := hashes[i%max]
-		_, _ = prod_hub_ops.MDQ("{sha1}" + hash)
-	}
+func ExampleMDQFilter() {
+	_, numberOfTestSPs, _ := Md.Internal.(*MDQ).MDQFilter("/*[not(contains(@entityID, 'birk.wayf.dk/birk.php'))]/*/wayf:wayf[not(wayf:IDPList!='') and wayf:redirect.validate='']/../../md:SPSSODescriptor/..")
+	fmt.Println(numberOfTestSPs)
+	// Output:
+	// 121
 }
