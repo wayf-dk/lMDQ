7a1f651be042be984cd3a1001cf8838259da517f
Mads Freek <freek@wayf.dk>
2025-12-01 10:53:02 +0100

    Added support for:
    Letting the mddb do the check for common federations
    Providing MDQ multi feeds i.e. based on which sps/idps a(n) idp/sp is allowed to talk to i.e. have a non-empty federation intersection list with

--- lmdq.go
+++ lmdq.go
@@ -21,16 +21,16 @@ package lmdq
 */
 
 import (
+    "bytes"
 	"crypto/sha1"
 	"database/sql"
-	"encoding/hex"
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"regexp"
 	"sort"
-	"strings"
 	"sync"
 	"time"
 
@@ -51,14 +51,13 @@ type (
 	MDQ struct {
 		config.MdDb
 		db    *sql.DB
-		stmt  *sql.Stmt
+		onestmt, multistmt, selfstmt  *sql.Stmt
 		Cache map[string]*MdXp
 		Lock  sync.RWMutex
 	}
 	// MdXp refers to check validity
 	MdXp struct {
 		*goxml.Xp
-		xml     []byte
 		created time.Time
 	}
 )
@@ -81,11 +80,9 @@ func (xp *MdXp) Valid(duration time.Duration) bool {
 func (mdq *MDQ) Open() (err error) {
 	mdq.Lock.Lock()
 	defer mdq.Lock.Unlock()
-
 	if mdq.Mdq != "" {
 		return
 	}
-
 	mdq.Cache = make(map[string]*MdXp)
 	if mdq.db != nil {
 		mdq.db.Close()
@@ -94,8 +91,60 @@ func (mdq *MDQ) Open() (err error) {
 	if err != nil {
 		return
 	}
-	mdq.stmt, err = mdq.db.Prepare("select e.md md from entity_" + mdq.Table + " e, lookup_" + mdq.Table + " l where ? < l.hash||'z' and l.hash||'z' <= ? and l.entity_id_fk = e.id")
-	// This is supposed to be a very smart wayf do to prefix search - keep an eye on whether a 10 char prefix ie. using 40 bits is enough
+
+    findEntity := ` ? < l.hash||'z' AND l.hash||'z' <= ?||'z' `
+
+	mdq.multistmt, err = mdq.db.Prepare(`SELECT
+    e.md
+FROM
+    entity_` + mdq.Table + ` e
+WHERE
+    e.id IN
+    (   SELECT
+            entity_id_fk
+        FROM
+            fed_` + mdq.Table + ` f
+        WHERE
+            f.fed IN
+            (   SELECT
+                    f.fed
+                FROM
+                    fed_` + mdq.TableRev + `    f,
+                    lookup_` + mdq.TableRev + ` l
+                WHERE ` + findEntity + `AND l.entity_id_fk = f.entity_id_fk ))`)
+
+    mdq.onestmt, err = mdq.db.Prepare(`SELECT
+    e.md
+FROM
+    (   SELECT
+            e.md
+        FROM
+            entity_` + mdq.Table + ` e,
+            lookup_` + mdq.Table + ` l
+        WHERE
+           ` + findEntity + `
+            AND l.entity_id_fk = e.id) e,
+    (   SELECT
+            f.fed
+        FROM
+            lookup_` + mdq.Table + ` l,
+            fed_` + mdq.Table + `    f
+        WHERE
+            ` + findEntity + `
+            AND l.entity_id_fk = f.entity_id_fk
+
+        INTERSECT
+
+        SELECT
+            f.fed
+        FROM
+            lookup_` + mdq.TableRev + ` l,
+            fed_` + mdq.TableRev + `    f
+        WHERE
+            ` + findEntity + `
+            AND l.entity_id_fk = f.entity_id_fk ) i;
+`)
+	mdq.selfstmt, err = mdq.db.Prepare(`select e.md md from entity_` + mdq.Table + ` e where e.id in (SELECT l.entity_id_fk FROM lookup_` + mdq.Table + ` l WHERE` + findEntity +`)`)
 	if err != nil {
 		return
 	}
@@ -110,40 +159,78 @@ func (mdq *MDQ) Open() (err error) {
 // This might be an optimization as the database lookup is much faster that the parsing.
 func (mdq *MDQ) MDQ(key string) (xp *goxml.Xp, err error) {
 	if mdq.Mdq == "" {
-		xp, _, err = mdq.dbget(key, true)
+	    return mdq.WebMDQ(key, key)
+//		return mdq.dbget(key, true)
 	} else {
-		xp, _, err = mdq.mdqget(key, true)
+		return mdq.mdqget(key, true)
 	}
+}
+
+func c14n(key string) (string) {
+    if hexChars.MatchString(key) || key == "" {
+        // already sha1'ed - do nothing
+	} else {
+	    key = fmt.Sprintf("%x", sha1.Sum([]byte(key)))
+	}
+    return key
+}
+
+func (mdq *MDQ) WebMDQ(key, key2 string) (xp *goxml.Xp, err error) {
+    var rows *sql.Rows
+    key, key2 = c14n(key), c14n(key2)
+    if key == key2 {
+      	rows, err = mdq.selfstmt.Query(key, key)
+    } else if key2 != "" {
+        rows, err = mdq.onestmt.Query(key2, key2, key2, key2, key, key)
+    } else {
+    	rows, err = mdq.multistmt.Query(key, key)
+    }
+	if err != nil {
+		return
+	}
+	defer rows.Close()
+	md, c := []byte{}, 0
+	for rows.Next() {
+		var buf []byte
+		if err = rows.Scan(&buf); err != nil {
+			return
+		}
+		prefix := `<?xml version="1.0" encoding="UTF-8"?>
+`
+		entitymd, _ := bytes.CutPrefix(gosaml.Inflate(buf), []byte(prefix))
+		md = append(md, entitymd...)
+		c++
+	}
+	if c == 0 {
+	    return xp, MetaDataNotFoundError
+	}
+	if c >= 2 {
+        md = append([]byte(`<?xml version="1.0" encoding="UTF-8"?><md:EntitiesDescriptor xmlns:md="urn:oasis:names:tc:SAML:2.0:metadata">`), md...)
+        md = append(md, `</md:EntitiesDescriptor>`...)
+    }
+	xp = goxml.NewXp(md)
+    if c == 1 {
+    	testify(xp)
+    }
+    for _, signature := range xp.Query(nil, "./md:EntityDescriptor/ds:Signature") {
+        xp.Rm(signature, ".")
+    }
 	return
 }
 
-// WebMDQ - Export of dbget
-func (mdq *MDQ) WebMDQ(key string) (xp *goxml.Xp, xml []byte, err error) {
-	return mdq.dbget(key, true)
-}
-
-func (mdq *MDQ) dbget(key string, cache bool) (xp *goxml.Xp, xml []byte, err error) {
-	k := key
-	if strings.HasPrefix(key, "{sha1}") {
-		key = key[6:]
-	} else if hexChars.MatchString(key) {
-		// already sha1'ed - do nothing
-	} else {
-		hash := sha1.Sum([]byte(key))
-		key = hex.EncodeToString(append(hash[:]))
-	}
-	//key = key[:10] // only use the first 10 chars for key - why on earth do that???
+func (mdq *MDQ) dbget(key string, cache bool) (xp *goxml.Xp, err error) {
+    k := key
+    key = c14n(key)
 	mdq.Lock.RLock()
 	cachedxp := mdq.Cache[key]
 	if cachedxp != nil && cachedxp.Valid(cacheduration) {
 		xp = cachedxp.Xp.CpXp()
-		xml = cachedxp.xml
 		mdq.Lock.RUnlock()
 		return
 	}
 	mdq.Lock.RUnlock()
-
-	err = mdq.stmt.QueryRow(key, key+"z").Scan(&xml)
+    var xml []byte
+	err = mdq.selfstmt.QueryRow(key, key).Scan(&xml)
 	switch {
 	case err == sql.ErrNoRows:
 		err = goxml.Wrap(MetaDataNotFoundError, "err:Metadata not found", "key:"+k, "table:"+mdq.Short)
@@ -158,7 +245,6 @@ func (mdq *MDQ) dbget(key string, cache bool) (xp *goxml.Xp, xml []byte, err err
 	if cache {
 		mdxp := new(MdXp)
 		mdxp.Xp = xp
-		mdxp.xml = xml
 		mdxp.created = time.Now()
 		mdq.Lock.Lock()
 		mdq.Cache[key] = mdxp
@@ -167,7 +253,7 @@ func (mdq *MDQ) dbget(key string, cache bool) (xp *goxml.Xp, xml []byte, err err
 	return
 }
 
-func (mdq *MDQ) mdqget(key string, cache bool) (xp *goxml.Xp, xml []byte, err error) {
+func (mdq *MDQ) mdqget(key string, cache bool) (xp *goxml.Xp, err error) {
 	client := &http.Client{}
 	q := mdq.Mdq + url.PathEscape(key)
 	req, _ := http.NewRequest("GET", q, nil)
@@ -175,17 +261,17 @@ func (mdq *MDQ) mdqget(key string, cache bool) (xp *goxml.Xp, xml []byte, err er
 
 	if err != nil || response.StatusCode == 500 {
 		err = goxml.Wrap(MetaDataNotFoundError, "err:Metadata not found", "key:"+key, "table:"+mdq.Short)
-		return nil, nil, err
+		return nil, err
 	}
 
 	defer response.Body.Close()
-	xml, err = ioutil.ReadAll(response.Body)
+	xml, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 	//md := gosaml.Inflate(xml)
 	xp = goxml.NewXp(xml)
-	return xp, xml, err
+	return xp, err
 }
 
 // MDQFilter refers Filtering by xpath for testing purposes
@@ -209,8 +295,7 @@ func (mdq *MDQ) MDQFilter(xpathfilter string) (xp *goxml.Xp, numberOfEntities in
 
 	root, _ := xp.Doc.DocumentElement()
 	for _, entityID := range index {
-		ent, _, _ := mdq.dbget(entityID, false)
-
+		ent, _ := mdq.dbget(entityID, false)
 		if xpathfilter == "" || len(ent.Query(nil, xpathfilter)) > 0 {
 			entity, _ := ent.Doc.DocumentElement()
 			root.AddChild(xp.CopyNode(entity, 1))
